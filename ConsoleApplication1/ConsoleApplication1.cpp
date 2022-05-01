2b93883ad78196b77bcc0bc29d4a1f2e62e97ee2
matija2004 <matijacetrovic@gmail.com>
2022-05-01 09:22:17 +0200

    added calendar

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -108,11 +108,66 @@ void put_date(char clock[39][79], int day, int month, int year)
 
 	char date[15];
 
-	sprintf_s(date, "%0d. %s, %d.", day, months[month], year);
+	sprintf_s(date, "%02d. %s, %d.", day, months[month], year);
 
 	for (int i = 0; i < 14; i++)
 	{
 		clock[9][i + 32] = date[i];
+	}
+}
+
+void fill_callendar(char calendar[13][35], int day, int month, int year)
+{
+	int day_of_the_week;
+	int n_of_days_in_a_month[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+		n_of_days_in_a_month[1]++;
+	year -= month < 2;
+	day_of_the_week = (year + year / 4 - year / 100 + year / 400 + "-bed=pen+mad."[month+1] + day - 1) % 7;
+	int dayt = day;
+	
+	while (dayt > 1)
+	{
+		dayt --;
+		day_of_the_week--;
+		if (day_of_the_week < 0)
+		{
+			day_of_the_week += 7;
+		}
+	}
+	
+	if (day_of_the_week > 0)
+	{
+		dayt -= day_of_the_week;
+		month--;
+		dayt += n_of_days_in_a_month[month];
+	}
+	
+	for (int i = 0; i < 6; i++)
+	{
+		for (int j = 0; j < 7; j++)
+		{
+			char x[3];
+			sprintf_s(x, "%02d", dayt);
+			if (dayt == day)
+			{
+				calendar[i * 2 + 2][j * 5 + 1] = 'x';
+				calendar[i * 2 + 2][j * 5 + 2] = 'x';
+			}
+			else
+			{
+				calendar[i * 2 + 2][j * 5 + 1] = x[0];
+				calendar[i * 2 + 2][j * 5 + 2] = x[1];
+			}
+			
+			dayt++;
+			if (dayt > n_of_days_in_a_month[month])
+			{
+				month++;
+				dayt = 1;
+			}
+		}
 	}
 }
 
@@ -161,18 +216,33 @@ int main()
 		"                        ######        VI        ######                        ",
 		"                              ##################                              "
 	};
-
+	char calendar[13][35] = {
+		" MO | TU | WE | TH | FR | ST | SU ",
+		"----+----+----+----+----+----+----",
+		"    |    |    |    |    |    |    ",
+		"----+----+----+----+----+----+----",
+		"    |    |    |    |    |    |    ",
+		"----+----+----+----+----+----+----",
+		"    |    |    |    |    |    |    ",
+		"----+----+----+----+----+----+----",
+		"    |    |    |    |    |    |    ",
+		"----+----+----+----+----+----+----",
+		"    |    |    |    |    |    |    ",
+		"----+----+----+----+----+----+----",
+		"    |    |    |    |    |    |    "
+	};
 	int x[3], y[3];
 
 	int tHours;
 	std::cout << "Enter your timezone : UTC";
 	std::cin >> tHours;
-
+	std::cout << "\033[?25l";
 begin:
 	if (!get_time(day, month, year, hours, minutes, seconds, tHours))
 	{
 		//system("cls");
 		std::cout << "\033[0;0H";
+		fill_callendar(calendar, day, month, year);
 		put_date(clock, day, month, year);
 		get_point_for_seconds(seconds, x[0], y[0]);
 		draw_line(clock, 39, 78, x[0], y[0], '*');
@@ -180,15 +250,90 @@ begin:
 		draw_line(clock, 39, 78, x[1], y[1], '+');
 		get_point_for_hours(hours, x[2], y[2]);
 		draw_line(clock, 39, 78, x[2], y[2], '@');
+
+		int t = 0;
+		char z[3];
+		sprintf_s(z, "%02d", day);
 		for (int i = 0; i < 39; i++)
 		{
 			for (int j = 0; j < 79; j++)
 			{
-				if (clock[i][j] != '#')
-					std::cout << clock[i][j];
-				else
+				if (clock[i][j] == '#')
 					std::cout << '\333';
+				else if (clock[i][j] == '*')
+					std::cout << '\260';
+				else if (clock[i][j] == '+')
+					std::cout << '\261';
+				else if (clock[i][j] == '@')
+					std::cout << '\262';
+				else
+					std::cout << clock[i][j];
+					
 			}
+			std::cout << "  ";
+			if (i == 0)
+			{
+				char MONTH[12][10] = {
+					"JANUARY  ",
+					"FEBRUARY ",
+					"MARCH    ",
+					"APRIL    ",
+					"MAY      ",
+					"JUNE     ",
+					"JULY     ",
+					"AUGUST   ",
+					"SEPTEMBER",
+					"OCTOBER  ",
+					"NOVEMBER ",
+					"DECEMBER "
+				};
+				char t[36];
+				sprintf_s(t, " %s                  %04d. ", MONTH[month], year);
+				std::cout << t;
+			}
+			else if (i > 0 && i < 14)
+			{
+				for (int j = 0; j < 35; j++)
+				{
+					if (calendar[i-1][j] == 'x')
+					{
+						if (t == 0)
+						{
+							std::cout << "\033[32m" << z[0];
+							t++;
+						}
+						else if (t == 1)
+						{
+							std::cout << z[1] << "\033[0m";
+							t++;
+						}
+						else if (t == 2)
+						{
+							std::cout << z[0];
+							t++;
+						}
+						else
+						{
+							std::cout << z[1];
+							t++;
+						}
+						
+					}
+					else if (i == 1 && calendar[i-1][j] != ' ' && calendar[i-1][j] != '|')
+					{
+						std::cout << "\033[38;2;160;160;160m" << calendar[i - 1][j] << "\033[0m";
+					}
+					else if (calendar[i-1][j] == '|' || calendar[i-1][j] == '-' || calendar[i-1][j] == '+')
+					{
+						std::cout << "\033[38;2;128;128;128m" << calendar[i-1][j] << "\033[0m";
+					}
+					else
+					{
+						std::cout << calendar[i-1][j];
+					}
+				}
+			}
+
 			std::cout << std::endl;
 		}
 		draw_line(clock, 39, 78, x[0], y[0], ' ');
